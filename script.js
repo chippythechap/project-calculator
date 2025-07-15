11b014ddc43c512f103a4da2b535d70ac87f9991
chippythechap <chippythechap@gmail.com>
2025-07-15 11:36:30 +0200

    start: buttons and basic math-functions created

---
+++ script.js
@@ -0,0 +1,38 @@
+let a;
+let b;
+let operator;
+
+//functions
+
+function operate(a,b,operator) {
+    switch (operator) {
+        case "+":
+            add(a,b);
+            break;
+        case "-":
+            subtract(a,b);
+            break;
+        case "*":
+            multiply(a,b);
+            break;
+        case "/":
+            divide(a,b);
+            break;
+    }
+}
+
+function add(a,b) {
+    return a + b;
+}
+
+function subtract(a,b) {
+    return a - b;
+}
+
+function multiply(a,b) {
+    return a * b;
+}
+
+function divide(a,b) {
+    return a / b;
+}
